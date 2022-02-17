2c92e51a5f6ed417a4fb0adcf43a3cc0c2346367
Pegipin <74768438+Pegipin@users.noreply.github.com>
2022-02-17 11:56:59 +0100

    Add files via upload

---
+++ The food order App-React/src/components/UI/Input.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './Input.module.css'
+
+const Input=React.forwardRef(props) =>{
+  return <div className={classes.inp,refut}>
+   <label htmlFor={props.input.id}>{props.label}</label>
+   <input ref={ref} {...props.input}/>
+  </div>;
+}
+
+export default Input;
